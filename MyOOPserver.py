618167ce45a5eeb9aa2c434bea41b282f5954f34
marzieh <marziehsepehr@gmai.com>
2019-12-08 21:01:46 +0330

    bug fix:close pv do not close other connection solved

--- MyOOPserver.py
+++ MyOOPserver.py
@@ -13,6 +13,7 @@ class ChatServer(object):
 
     def __init__(self,IP,PORT,server_capacity):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
         self.server_socket.setblocking(0)
         print(color_message.BOLD + color_message.OKBLUE + 'server is starting up on %s port %s....' % (IP, PORT) + color_message.ENDC)
         self.server_socket.bind((IP,PORT))
@@ -31,78 +32,81 @@ class ChatServer(object):
                 break
         return is_duplicated
 
-    def authenticate_user(self,client_socket):
-        while client_socket not in self.clients_sessions:
-            username = client_socket.recv(1024)
-            if not self.check_duplicated_user(username):
-
-                # do if username is not repeatative
-                self.input_sockets.append(client_socket)
-                self.clients_sessions[client_socket] = {}
-                client_socket.send(bytes('--------------welcome to chatroom!----------------', "UTF-8"))
-                break
-            else:
-                client_socket.send(bytes('your username is duplicated!', "UTF-8"))
+    # # TODO: has bug should fix in next stable version
+    # def authenticate_user(self,client_socket):
+    #     while client_socket not in self.clients_sessions:
+    #         username = client_socket.recv(1024)
+    #         if not self.check_duplicated_user(username):
+    #
+    #             # do if username is not repeatative
+    #             self.input_sockets.append(client_socket)
+    #             self.clients_sessions[client_socket] = {}
+    #             client_socket.send(bytes('--------------welcome to chatroom!----------------', "UTF-8"))
+    #             break
+    #         else:
+    #             client_socket.send(bytes('your username is duplicated!', "UTF-8"))
     def run(self):
-        while self.input_sockets:
-            self.readble, _, self.exceptional = select.select(
-                self.input_sockets, [], self.input_sockets)
-            for s in self.readble:
-                if s is self.server_socket:
-                    client_socket, client_address = s.accept()
-                    print(color_message.OKGREEN + "{0} enter the server...".format(client_address) + color_message.ENDC)
-                    client_socket.setblocking(0)
-                    self.input_sockets.append(client_socket)
-                    client_socket.send(bytes('welcom...Enter your username', "UTF-8"))
-                    self.clients_sessions[client_socket] = {}
-                    # thread_auth=threading.Thread(target=self.authenticate_user,args=[client_socket])
-                    # thread_auth.start()
-                    # # do if username is not repeatative
-                    # self.input_sockets.append(client_socket)
-                    # self.clients_sessions[client_socket] = {}
-                    # client_socket.send(bytes('welcom...Enter your username', "UTF-8"))
+       try:
+           while self.input_sockets:
+               self.readble, _, self.exceptional = select.select(
+                   self.input_sockets, [], self.input_sockets)
+               for s in self.readble:
+                   if s is self.server_socket:
+                       client_socket, client_address = s.accept()
+                       print(color_message.OKGREEN + "{0} enter the server...".format(
+                           client_address) + color_message.ENDC)
+                       client_socket.setblocking(0)
+                       self.input_sockets.append(client_socket)
+                       self.clients_sessions[client_socket] = {}
+                       client_socket.send(bytes('welcom...Enter your username', "UTF-8"))
 
-                else:
-                    try:
-                        message=s.recv(1024).decode("UTF-8")
-                    except Exception as a:
-                        print(a)
-                    if message:
-                        self.message_handler(s,message)
-                    # A readable socket without data available is from a client that
-                    # has disconnected, and the stream is ready to be closed.
-                    else:
-                        print(color_message.YELLOW + "{0} left the room".format(s.getpeername()) + color_message.ENDC)
-                        # TODO: remove user pv variable
-                        # ----------------------------------------
-                        # tell others someone left the room
-                        for client in self.input_sockets:
-                            if 'pv' not in self.clients_sessions[s]:
-                                if client != self.server_socket and client != s:
-                                    client.send(bytes(color_message.RED + "{0} left the room...".format(
-                                        self.clients_sessions[s]['uname']) + color_message.ENDC, "utf-8"))
-                            else:
-                                target = self.clients_sessions[s]['pv']
-                                target.send(bytes(color_message.RED + "------{0} left the pv-------".format(
-                                    self.clients_sessions[s]['uname']) + color_message.ENDC, "utf-8"))
-                                try:
-                                    del self.clients_sessions[target]['pv']
-                                except KeyError:
+                       # thread_auth=threading.Thread(target=self.authenticate_user,args=[client_socket])
+                       # thread_auth.start()
 
-                                    print("error")
-                        # if s have someone in pv remove it
 
-                        del self.clients_sessions[s]
-                        # remove user from room
-                        self.input_sockets.remove(s)
-                        s.close()
+                   else:
+                       try:
+                           message = s.recv(1024).decode("UTF-8")
+                       except Exception as a:
+                           print(a)
+                       if message:
+                           self.message_handler(s, message)
+                       # A readable socket without data available is from a client that
+                       # has disconnected, and the stream is ready to be closed.
+                       else:
+                           print(color_message.YELLOW + "{0} left the room".format(s.getpeername()) + color_message.ENDC)
+                           # TODO: remove user pv variable
+                           # ----------------------------------------
+                           # tell others someone left the room
+                           for client in self.input_sockets:
 
-            for s in self.exceptional:
-                loger('log.txt', '{0} had problem in connection'.format(s))
-                self.input_sockets.remove(s)
-                del self.clients_sessions[s]
-                s.close()
+                               if client in self.clients_sessions and 'pv' not in self.clients_sessions[client]:
+                                   if client != self.server_socket and client != s:
+                                       client.send(bytes(color_message.RED + "{0} left the room...".format(
+                                           self.clients_sessions[s]['uname']) + color_message.ENDC, "utf-8"))
+                               else:
+                                   target = self.clients_sessions[s]['pv']
+                                   target.send(bytes(color_message.RED + "------{0} left the pv-------".format(
+                                       self.clients_sessions[s]['uname']) + color_message.ENDC, "utf-8"))
+                                   try:
+                                       del self.clients_sessions[target]['pv']
+                                   except KeyError:
 
+                                       print("error")
+                           # if s have someone in pv remove it
+
+                           del self.clients_sessions[s]
+                           # remove user from room
+                           self.input_sockets.remove(s)
+                           s.close()
+
+               for s in self.exceptional:
+                   loger('log.txt', '{0} had problem in connection'.format(s))
+                   self.input_sockets.remove(s)
+                   del self.clients_sessions[s]
+                   s.close()
+       except Exception as ex:
+           self.server_socket.close()
     def find_client(self,username):
         for k in self.clients_sessions:
             if self.clients_sessions[k]['uname'] == username:
@@ -120,13 +124,28 @@ class ChatServer(object):
         #situation 1 : message: username
         if 'uname' not in self.clients_sessions[source]:
 
-            # TODO: check for duplicated
+
             self.clients_sessions[source]['uname'] = message
-            source.send(bytes("your username set as :{0}".format(message),"UTF-8"))
+            source.send(bytes('--------------welcome to chatroom {0} !----------------'.format(message), "UTF-8"))
             for client in self.input_sockets:
                 if client != self.server_socket and client != source:
                     client.send(bytes(color_message.OKGREEN + "{0} enter the room...".format(
                         self.clients_sessions[source]['uname']) + color_message.ENDC, "utf-8"))
+            # TODO: check for duplicated
+            # if not self.check_duplicated_user(message):
+            #     self.input_sockets.append(source)
+            #     self.clients_sessions[source] = {}
+            #     self.clients_sessions[source]['uname'] = message
+            #     source.send(bytes('--------------welcome to chatroom {0} !----------------'.format(message), "UTF-8"))
+            #     # source.send(bytes("your username set as :{0}".format(message),"UTF-8"))
+            #     for client in self.input_sockets:
+            #         if client != self.server_socket and client != source:
+            #             client.send(bytes(color_message.OKGREEN + "{0} enter the room...".format(
+            #                 self.clients_sessions[source]['uname']) + color_message.ENDC, "utf-8"))
+            # else:
+            #     source.send(bytes('your username is duplicated!', "UTF-8"))
+
+
 
 
         else:
@@ -174,7 +193,7 @@ class ChatServer(object):
 
 if __name__ == "__main__":
 
-    server=ChatServer('192.168.43.65',9032,100)
+    server=ChatServer('192.168.43.65',9001,100)
     server.run()
 
 
