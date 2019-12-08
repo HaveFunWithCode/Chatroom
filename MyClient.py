a231b5d6e6b691beced4b6b5d29e8ae3d0598dec
marzieh <marziehsepehr@gmai.com>
2019-12-08 21:01:10 +0330

    clean client code

--- MyClient.py
+++ MyClient.py
@@ -1,49 +1,47 @@
-import os
 import socket
 import sys
 import threading
-import subprocess
 
-IP='192.168.1.141'
-PORT=9031
+class ChatClient(object):
+    def __init__(self,IP,PORT):
+        self.IP=IP
+        self.PORT=PORT
+        self.client_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.client_socket.connect((IP,PORT))
+        self.client_socket.setblocking(False)
 
-client_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-client_socket.connect((IP,PORT))
-client_socket.setblocking(False)
-
-def send_message():
-    print('for private message to someone, write call:name')
-    while True:
-        msg=input('->')
-        if msg:
-            client_socket.send(bytes(msg,"utf-8"))
-
-# what is the size of chunck
-# message=client_socket.recv(1024)
-# print(message.decode('utf-8'))
-t1=threading.Thread(target=send_message)
-t1.start()
-# TODO: ask! why 2 while?!
-
-while True:
-    try:
+    def send_message(self):
+        print('for private message to a username, write: call:username')
         while True:
-            message = client_socket.recv(1024)
-            if not message:
-                print('connection close!')
-                sys.exit()
-            if message.decode("utf-8").startswith("call:"):
-                pvname = message[5:]
-                # os.system('clear')
-                print("*--------------------------------------*")
-                print('{0} pv->>'.format(pvname))
-            else:
-                print(message.decode("utf-8"))
-    except IOError:
-        pass
-    # print(message.decode("utf-8"))
-    # msg=input('->')
-    # client_socket.send(bytes(msg,"utf-8"))
+            msg=input('->')
+            if msg:
+                self.client_socket.send(bytes(msg,"utf-8"))
+
+
+    def run(self):
+        t1 = threading.Thread(target=self.send_message)
+        t1.start()
+
+        while True:
+            try:
+                while True:
+                    message = self.client_socket.recv(1024)
+                    if not message:
+                        print('connection close!')
+                        sys.exit()
+                    if message.decode("utf-8").startswith("call:"):
+                        pvname = message.decode("utf-8")[5:]
+                        print("*-----------------{0}----------------*".format(pvname))
+
+                    else:
+                        print(message.decode("utf-8"))
+            except IOError:
+                pass
+
+if __name__=='__main__':
+    client=ChatClient('192.168.43.65',9001)
+    client.run()
+
 
 
 
